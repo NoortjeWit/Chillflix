330519dfd52827bd7534b28c700a7294f59e973b
ChantalSchot <chantalschot@outlook.com>
2020-03-04 15:18:07 +0100

    converted video component to interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
+import { IVideo } from './video-module/video/video.interface';
 import { videos } from './videos';
-import { VideoComponent } from './video-module/video/video.component';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  videos: VideoComponent[] = videos;
+  videos: IVideo[] = videos;
 }
