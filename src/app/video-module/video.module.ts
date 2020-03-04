2c2bc654653d222f72aa201a2e9980649c1c7451
ChantalSchot <chantalschot@outlook.com>
2020-03-04 13:23:56 +0100

    updated video module

--- src/app/video-module/video.module.ts
+++ src/app/video-module/video.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { YouTubePlayerModule } from '@angular/youtube-player';
-import { YoutubePlayerComponent } from './youtube-component/youtube-player.component';
+import { YoutubePlayerComponent } from './youtube-player/youtube-player.component';
 import { VideoComponent } from './video/video.component';
+import { VideoListComponent } from './video-list/video-list.component';
 
 @NgModule({
   imports: [
     CommonModule,
     YouTubePlayerModule
   ],
-  declarations: [YoutubePlayerComponent, VideoComponent],
+  declarations: [YoutubePlayerComponent, VideoComponent, VideoListComponent],
   exports: [YoutubePlayerComponent, VideoComponent]
 })
 export class VideoModule { }
