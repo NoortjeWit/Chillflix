59564e769a8c42cf0e638b37684b67e845bd4f7c
ChantalSchot <chantalschot@outlook.com>
2020-03-09 16:29:26 +0100

    implemented router

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const appRoutes: Routes = [
+  { path: "home", component: AppComponent },
+  { path: "", redirectTo: "/home", pathMatch: "full" },
+  { path: "**", component: NotFoundComponent },
+]
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(
+      appRoutes, { enableTracing: true },
+    )
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
