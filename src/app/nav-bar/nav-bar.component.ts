0bc756878a3f5dc195d5cc2bf328bbe14bf2af86
Noortje Wittink <noortje.wittink@hotmail.com>
2020-03-11 16:25:41 +0100

    suggestion box

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,18 +1,43 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
-import { LoginService } from '../shared/login.service';
+import { Component, OnInit, EventEmitter, Output } from "@angular/core";
+import { LoginService } from "../shared/login.service";
+import { videos } from "../shared/stubs/videos";
+import { IVideo } from "../video-module/video/video.interface";
+import { PreviewService } from "../shared/preview.service";
 
 @Component({
-  selector: 'app-nav-bar',
-  templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css']
+  selector: "app-nav-bar",
+  templateUrl: "./nav-bar.component.html",
+  styleUrls: ["./nav-bar.component.css"]
 })
 export class NavBarComponent implements OnInit {
+  videoSuggestion: IVideo;
+  videoSuggestion2: IVideo;
 
-
-  constructor(public loginService: LoginService) { }
+  constructor(
+    public loginService: LoginService,
+    public previewService: PreviewService
+  ) {}
 
   ngOnInit() {
+    const num = this.getRandomInt(videos.length);
+    this.videoSuggestion = videos[num];
+    this.videoSuggestion2 = videos[this.getRandomInt(videos.length, num)];
   }
 
+  selectVideo(video) {
+    this.previewService.addSelectedVideo(video);
+    this.previewService.fullDescription = false;
+    this.previewService.descriptionText = "show full description";
+    window.scroll(0, 0);
+  }
 
+  getRandomInt(max, num?) {
+    let rand = Math.floor(Math.random() * Math.floor(max));
+    if (num) {
+      while (rand == num) {
+        rand = Math.floor(Math.random() * Math.floor(max));
+      }
+    }
+    return rand;
+  }
 }
--- src/app/nav-bar/nav-bar.module.ts
+++ src/app/nav-bar/nav-bar.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
+import { VideoModule} from "../video-module/video.module";
 
 
 @NgModule({
   imports: [
     CommonModule,
-    AppRoutingModule
+    AppRoutingModule,
+    VideoModule
   ],
   declarations: [NavBarComponent, SearchBarComponent, LoginComponent],
   exports: [NavBarComponent, SearchBarComponent, LoginComponent]
