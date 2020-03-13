f53cfe70e276f358afbd3a280710954d84d4fbdd
ChantalSchot <chantalschot@outlook.com>
2020-03-13 14:44:43 +0100

    preview test and interceptor bugfix

--- src/app/shared/oauth-token-header.interceptor.ts
+++ src/app/shared/oauth-token-header.interceptor.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpHeaders } from '@angular/common/http';
 
 import { Observable } from 'rxjs';
+import { isUndefined } from 'util';
 
 @Injectable()
 export class AuthTokenInterceptor implements HttpInterceptor {
@@ -12,19 +13,17 @@ export class AuthTokenInterceptor implements HttpInterceptor {
 
 	intercept(req: HttpRequest<any>, next: HttpHandler):
 		Observable<HttpEvent<any>> {
-		if (this.loginService.user) {
+		if (this.loginService.user !== null && this.loginService.user !== undefined) {
 			this.token = this.loginService.user.authToken;
-			console.log(this.loginService.user);
+			console.log("test")
 			if (req.url.includes("https://www.googleapis.com/youtube")) {
-				req = req.clone(
-					{
-            headers: req.headers.set('Authorization', `Bearer ${this.token}`).set('Accept', 'application/json')
-					}
-				)
+				req = req.clone({
+					headers: req.headers.set('Authorization', `Bearer ${this.token}`).set('Accept', 'application/json')
+				})
 				console.log(req.headers);
 			};
-			return next.handle(req);
 		}
+		return next.handle(req);
 	};
 
 }
--- src/app/video-module/preview/preview.component.spec.ts
+++ src/app/video-module/preview/preview.component.spec.ts
@@ -1,3 +1,7 @@
+import { ShortDescriptionPipe } from './../../shared/short-description.pipe';
+import { VideoDurationPipe } from './../../shared/video-duration.pipe';
+import { PreviewService } from './../../shared/preview.service';
+import { IVideo } from './../video/video.interface';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PreviewComponent } from './preview.component';
@@ -6,9 +10,22 @@ describe('PreviewComponent', () => {
   let component: PreviewComponent;
   let fixture: ComponentFixture<PreviewComponent>;
 
+  let previewServiceStub: any = {
+    getSelectedVideo: () => { }
+  }
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [PreviewComponent]
+      declarations: [
+        PreviewComponent,
+        VideoDurationPipe,
+        ShortDescriptionPipe
+      ],
+      providers: [
+        {
+          provide: PreviewService, useValue: previewServiceStub
+        }
+      ]
     })
       .compileComponents();
   }));
@@ -17,9 +34,12 @@ describe('PreviewComponent', () => {
     fixture = TestBed.createComponent(PreviewComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    const previewSpy = spyOn(previewServiceStub, 'getSelectedVideo').and.returnValue({ videoId: "kjwe029", title: "Titanic" } as IVideo);
+
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 });
